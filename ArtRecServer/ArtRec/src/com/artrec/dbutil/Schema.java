286cce107cfdff753fb1bf81841a7d7d31eedb09
vsbirkehaug <vilde.birkehaug@students.plymouth.ac.uk>
2016-04-23 20:51:43 +0100

    Can now select subjects and get matching journals.

--- ArtRecServer/ArtRec/src/com/artrec/dbutil/Schema.java
+++ ArtRecServer/ArtRec/src/com/artrec/dbutil/Schema.java
@@ -1,11 +1,28 @@
 package com.artrec.dbutil;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.sql.*;
+import java.util.Iterator;
+import java.util.List;
+import java.util.zip.GZIPInputStream;
 
 import com.artrec.json.ConvertToJson;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.syndication.feed.synd.SyndEntry;
+import com.sun.syndication.feed.synd.SyndFeed;
+import com.sun.syndication.io.FeedException;
+import com.sun.syndication.io.SyndFeedInput;
 
 import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONObject;
+import org.xml.sax.InputSource;
 
 
 /**
@@ -36,17 +53,8 @@ public class Schema extends DbUtil {
 		try {
 			conn = connectToDatabase();									//Connect to the database
 			
-																		/*
-																		 * For prepared statements, strings, integers etc MUST be set.
-																		 * As we're only looking for UK airports with a country_code = 'GB' we have 
-																		 * the set parameter 1 as "GB" as a String.
-																		 * 
-																		 * The only time this doesn't apply is if a set integer is in the query string, so:
-																		 * "SELECT * FROM table WHERE id = 1"... but nobody ever uses that, as the ID will always be different...
-																		*/
-			
 			qry = conn
-					.prepareStatement("SELECT DISTINCT title FROM artrecdb.subjects ORDER BY title");		//Prepare the query searching for distinct subject names
+					.prepareStatement("SELECT * FROM artrecdb.subjects ORDER BY title");		//Prepare the query searching for distinct subject names
 			rs = qry.executeQuery();																		//Execute the prepared query
 			
 			jsonArray = jsonConverter.convertToJsonArray(rs);												//Convert the result set to JSON
@@ -68,4 +76,205 @@ public class Schema extends DbUtil {
 		return jsonArray;												//Return the JSON array
 			
 	}
+	
+	
+	public JSONArray qryAuthenticateUser(String username, String password) throws Exception{
+		
+		PreparedStatement qry = null;									//Create a statement that can later be used as an SQL call
+		Connection conn = null;											//Create a connection to that can be used to connect to the database
+		ResultSet rs = null;											//Define a variable for the resulting data
+		ConvertToJson jsonConverter = new ConvertToJson();				//Create a variable to convert the result set to JSON
+		JSONArray jsonArray = new JSONArray();							//Create a JSON array that will later be used as the output value
+		
+		try {
+			conn = connectToDatabase();									//Connect to the database
+			
+			qry = conn.prepareStatement("SELECT id FROM artrecdb.users WHERE username = ? AND password = ?");		//Prepare the query searching for distinct subject names
+			qry.setString(1, username);
+			qry.setString(2, password);
+			rs = qry.executeQuery();																		//Execute the prepared query
+			
+			jsonArray = jsonConverter.convertToJsonArray(rs);												//Convert the result set to JSON
+
+		} catch (SQLException e) {																			//Catch any Exceptions and print them to the console
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {											//If the result set is empty close it
+				rs.close();
+			}
+			if (qry != null) {											//If the query is empty close it.
+				qry.close();
+			}
+			if (conn != null) {											//If the connection is empty, close it.
+				conn.close();
+			}
+		}
+
+		return jsonArray;												//Return the JSON array
+			
+	}
+	
+public int qryInsertUser(String username, String password) throws Exception{
+		
+		PreparedStatement qry = null;									//Create a statement that can later be used as an SQL call
+		Connection conn = null;											//Create a connection to that can be used to connect to the database
+		int result = 0;											//Define a variable for the resulting data
+		ConvertToJson jsonConverter = new ConvertToJson();				//Create a variable to convert the result set to JSON
+		JSONArray jsonArray = new JSONArray();							//Create a JSON array that will later be used as the output value
+		
+		try {
+			conn = connectToDatabase();									//Connect to the database
+			
+			qry = conn.prepareStatement("INSERT INTO artrecdb.users (username, password) VALUES (?, ?)");		//Prepare the query searching for distinct subject names
+			qry.setString(1, username);
+			qry.setString(2, password);
+			result = qry.executeUpdate();																		//Execute the prepared query			
+										//Convert the result set to JSON
+
+		} catch (SQLException e) {																			//Catch any Exceptions and print them to the console
+			e.printStackTrace();
+		} finally {
+			
+			if (qry != null) {											//If the query is empty close it.
+				qry.close();
+			}
+			if (conn != null) {											//If the connection is empty, close it.
+				conn.close();
+			}
+		}
+
+		return result;												//Return the JSON array
+			
+	}
+
+	public JSONArray qryGetJournals() throws Exception {
+		PreparedStatement qry = null;									//Create a statement that can later be used as an SQL call
+		Connection conn = null;											//Create a connection to that can be used to connect to the database
+		ResultSet rs = null;											//Define a variable for the resulting data
+		ConvertToJson jsonConverter = new ConvertToJson();				//Create a variable to convert the result set to JSON
+		JSONArray jsonArray = new JSONArray();							//Create a JSON array that will later be used as the output value
+		
+		try {
+			conn = connectToDatabase();									//Connect to the database
+
+			qry = conn.prepareStatement("SELECT * FROM artrecdb.journals ORDER BY title");		//Prepare the query searching for distinct subject names
+			rs = qry.executeQuery();																		//Execute the prepared query
+			
+			jsonArray = jsonConverter.convertToJsonArray(rs);												//Convert the result set to JSON
+
+		} catch (SQLException e) {																			//Catch any Exceptions and print them to the console
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {											//If the result set is empty close it
+				rs.close();
+			}
+			if (qry != null) {											//If the query is empty close it.
+				qry.close();
+			}
+			if (conn != null) {											//If the connection is empty, close it.
+				conn.close();
+			}
+		}
+
+		return jsonArray;												//Return the JSON array
+			
+	}
+
+	public JSONArray qryGetArticlesForJournal(String journalISSN) {
+		JSONArray response = new JSONArray();
+		try {
+			SyndFeed feed = getSyndFeedForUrl("http://www.journaltocs.ac.uk/api/journals/"+journalISSN+"?output=articles&user=vilde2.birkehaug@live.uwe.ac.uk");
+			List entries = feed.getEntries();
+			Iterator<SyndEntry> itEntries = entries.iterator();
+			JSONObject json;
+			
+			while (itEntries.hasNext()) {
+				json = new JSONObject();
+				SyndEntry entry = itEntries.next();
+				json.append("title", entry.getTitle());
+				json.append("link", entry.getLink());
+				json.append("publicationDate", entry.getPublishedDate());
+				response.put(json);			
+			}
+		  } catch (Exception e) {
+
+			e.printStackTrace();
+
+		  }
+
+		return response;
+	}
+	
+	private String replaceChars(String string) {
+		return string.replace("[", "").replace("]", "").replace("\"", "").replace("\\", "");
+	}
+	
+	public SyndFeed getSyndFeedForUrl(String url) throws MalformedURLException, IOException, IllegalArgumentException, FeedException {
+		 
+		SyndFeed feed = null;		
+		InputStream is = null;
+	 
+		try {
+	 
+			URLConnection openConnection = new URL(url).openConnection();			
+			is = new URL(url).openConnection().getInputStream();
+			if("gzip".equals(openConnection.getContentEncoding())){
+				is = new GZIPInputStream(is);
+			}			
+			InputSource source = new InputSource(is);			
+			SyndFeedInput input = new SyndFeedInput();
+			feed = input.build(source);
+	 
+		} catch (Exception e){
+			System.out.println("Exception occured when building the feed object out of the url");				
+		} finally {
+			if( is != null)	is.close();
+		}
+	 
+		return feed; 
+	}
+
+
+	public JSONArray qryGetJournalsForSubjects(String ids) throws Exception {
+		PreparedStatement qry = null;									//Create a statement that can later be used as an SQL call
+		Connection conn = null;											//Create a connection to that can be used to connect to the database
+		ResultSet rs = null;											//Define a variable for the resulting data
+		ConvertToJson jsonConverter = new ConvertToJson();				//Create a variable to convert the result set to JSON
+		JSONArray jsonArray = new JSONArray();							//Create a JSON array that will later be used as the output value
+		
+		try {
+			conn = connectToDatabase();									//Connect to the database
+
+			qry = conn.prepareStatement("SELECT journals.idJournal, journals.title, journals.issn,journals.eissn "
+					+ "FROM journals "
+					+ "JOIN subject_journal "
+					+ "ON journals.idJournal = subject_journal.idJournal "
+					+ "JOIN subjects "
+					+ "ON subject_journal.idSubject = subjects.idSubject "
+					+ "WHERE subjects.idSubject IN (?)");		//Prepare the query searching for distinct subject names
+			qry.setString(1, ids);
+			rs = qry.executeQuery();																		//Execute the prepared query
+			
+			jsonArray = jsonConverter.convertToJsonArray(rs);												//Convert the result set to JSON
+
+		} catch (SQLException e) {																			//Catch any Exceptions and print them to the console
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {											//If the result set is empty close it
+				rs.close();
+			}
+			if (qry != null) {											//If the query is empty close it.
+				qry.close();
+			}
+			if (conn != null) {											//If the connection is empty, close it.
+				conn.close();
+			}
+		}
+
+		return jsonArray;												//Return the JSON array
+			
+		
+	}
+	
+	
 }
--- ArtRecServer/ArtRec/src/com/artrec/methods/ApiGetMethods.java
+++ ArtRecServer/ArtRec/src/com/artrec/methods/ApiGetMethods.java
@@ -1,20 +1,26 @@
 package com.artrec.methods;
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.nio.charset.Charset;
 
-
+import javax.servlet.http.Cookie;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
 
 import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONException;
@@ -22,47 +28,253 @@ import org.codehaus.jettison.json.JSONObject;
 
 import com.artrec.dbutil.Schema;
 
-
 @Path("/v1")
 public class ApiGetMethods {
-	
-	
-	//Default empty constructor so it can be called from the servlet
-	public ApiGetMethods() {}
-	
-	
+
+	// Default empty constructor so it can be called from the servlet
+	public ApiGetMethods() {
+	}
+
 	/**
 	 * 
 	 * @return JSON array of all subjects
 	 * 
-	 * The Get-All-Subjects Method retrieves a list of all of the subjects 
-	 * stored in the database. 
-	 * There are no parameters as all subjects are retrieved.
+	 *         The Get-All-Subjects Method retrieves a list of all of the
+	 *         subjects stored in the database. There are no parameters as all
+	 *         subjects are retrieved.
 	 * 
 	 */
 	@Path("/getAllSubjects")
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public JSONArray getAllSubjects(){
-	JSONArray jsonArray = new JSONArray();				//Creates a JSON array for all the data retrieved from the query,
-														//This will be returned to the application
-	Schema dao = new Schema();							//Gives the call access to the Schema allowing the SQL queries to be made
-	
-	try {												//Attempt a call to the database
-		jsonArray =  dao.qryGetSubjects();
-	} catch (Exception e) {								//Print out to the console if the call is not completed
-		e.printStackTrace();
+	public JSONArray getAllSubjects() {
+		JSONArray jsonArray = new JSONArray(); // Creates a JSON array for all
+												// the data retrieved from the
+												// query,
+												// This will be returned to the
+												// application
+		Schema dao = new Schema(); // Gives the call access to the Schema
+									// allowing the SQL queries to be made
+
+		try { // Attempt a call to the database
+			jsonArray = dao.qryGetSubjects();
+		} catch (Exception e) { // Print out to the console if the call is not
+								// completed
+			e.printStackTrace();
+		}
+
+		return jsonArray; // Returns the output from the SQL query
+	}
+
+	/**
+	 * 
+	 * @return JSON array of all journals
+	 * 
+	 *         The Get-All-Journals Method retrieves a list of all of the
+	 *         journals stored in the database. There are no parameters as all
+	 *         journals are retrieved.
+	 * 
+	 */
+	@Path("/getAllJournals")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public JSONArray getAllJournals() {
+		JSONArray jsonArray = new JSONArray(); // Creates a JSON array for all
+												// the data retrieved from the
+												// query,
+												// This will be returned to the
+												// application
+		Schema dao = new Schema(); // Gives the call access to the Schema
+									// allowing the SQL queries to be made
+
+		try { // Attempt a call to the database
+			jsonArray = dao.qryGetJournals();
+		} catch (Exception e) { // Print out to the console if the call is not
+								// completed
+			e.printStackTrace();
+		}
+
+		return jsonArray; // Returns the output from the SQL query
+	}
+
+	/**
+	 * 
+	 * @return JSON array of user id matching username and password
+	 * 
+	 *         The Get-User Method is used to authenticate the user
+	 * 
+	 */
+	@Path("/user")
+	@GET
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.APPLICATION_JSON)
+	public JSONArray getUser(@HeaderParam("username") String username, @HeaderParam("password") String password) {
+
+		try {
+			if (username != null && password != null) {
+				username = java.net.URLDecoder.decode(username, "UTF-8");
+				password = java.net.URLDecoder.decode(password, "UTF-8");
+			}
+		} catch (UnsupportedEncodingException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		System.out.println(username);
+		System.out.println(password);
+
+		JSONArray jsonArray = new JSONArray(); // Creates a JSON array for all
+												// the data retrieved from the
+												// query,
+												// This will be returned to the
+												// application
+		Schema dao = new Schema(); // Gives the call access to the Schema
+									// allowing the SQL queries to be made
+
+		try { // Attempt a call to the database
+			jsonArray = dao.qryAuthenticateUser(username.trim(), password.trim());
+			System.out.println(jsonArray.length() + "");
+		} catch (Exception e) { // Print out to the console if the call is not
+								// completed
+			e.printStackTrace();
+		}
+
+		return jsonArray; // Returns the output from the SQL query
 	}
 	
-	return jsonArray;									//Returns the output from the SQL query
+	/**
+	 * 
+	 *         The Get-User Method is used to authenticate the user
+	 * @throws UnsupportedEncodingException 
+	 * 
+	 */
+	@Path("/getJournalsForSubjects")
+	@GET
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.APPLICATION_JSON)
+	public JSONArray getJournalsForSubjects(@HeaderParam("ids") String ids) throws UnsupportedEncodingException {
+
+		ids = java.net.URLDecoder.decode(ids, "UTF-8");
+		System.out.println(ids);
+		
+//		String[] singleIds = ids.split(",");
+//		
+//		Integer[] newArray = new Integer[singleIds.length];
+//		int i = 0;
+//		for (String value : singleIds) {
+//		    newArray[i++] = Integer.valueOf(value);
+//		    System.out.print(value);
+//		}
+
+		JSONArray jsonArray = new JSONArray(); // Creates a JSON array for all
+												// the data retrieved from the
+												// query,
+												// This will be returned to the
+												// application
+		Schema dao = new Schema(); // Gives the call access to the Schema
+									// allowing the SQL queries to be made
+
+		try { // Attempt a call to the database
+			jsonArray = dao.qryGetJournalsForSubjects(ids);
+			System.out.println(jsonArray.length() + "");
+		} catch (Exception e) { // Print out to the console if the call is not
+								// completed
+			e.printStackTrace();
+		}
+
+		return jsonArray; // Returns the output from the SQL query
 	}
 	
 	
 	/**
+	 * 
+	 * @return JSON array of user id matching username and password
+	 * 
+	 *         The Get-User Method is used to authenticate the user
+	 * 
+	 */
+	@Path("/user")
+	@POST
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.APPLICATION_JSON)
+	public JSONArray InsertUser(@FormParam("username") String username, @FormParam("password") String password) {
+
+		try {
+			if (username != null && password != null) {
+				username = java.net.URLDecoder.decode(username, "UTF-8");
+				password = java.net.URLDecoder.decode(password, "UTF-8");
+			}
+		} catch (UnsupportedEncodingException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		System.out.println(username);
+		System.out.println(password);
+		System.out.println("inserting...");
+
+		JSONArray jsonArray = new JSONArray(); // Creates a JSON array for all
+												// the data retrieved from the
+												// query,
+												// This will be returned to the
+												// application
+		Schema dao = new Schema(); // Gives the call access to the Schema
+									// allowing the SQL queries to be made
+
+		try { // Attempt a call to the database
+			int result = dao.qryInsertUser(username.trim(), password.trim());
+			System.out.println(result + "");
+			jsonArray = dao.qryAuthenticateUser(username.trim(), password.trim());
+			System.out.println(jsonArray.length() + "");
+		} catch (Exception e) { // Print out to the console if the call is not
+								// completed
+			e.printStackTrace();
+		}
+
+		return jsonArray; // Returns the output from the SQL query
+	}
+	
+	
+
+	/**
+	 * 
+	 * @return JSON array of all journals
+	 * 
+	 *         The Get-All-Journals Method retrieves a list of all of the
+	 *         journals stored in the database. There are no parameters as all
+	 *         journals are retrieved.
+	 * 
+	 */
+	@Path("/getAllArticlesForJournal")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public JSONArray getAllArticlesForJournal(@Context UriInfo info) {
+		String journalISSN = info.getQueryParameters().getFirst("issn");
+
+		JSONArray jsonArray = new JSONArray(); // Creates a JSON array for all
+												// the data retrieved from the
+												// query,
+												// This will be returned to the
+												// application
+		Schema dao = new Schema(); // Gives the call access to the Schema
+									// allowing the SQL queries to be made
+
+		try { // Attempt a call to the database
+			jsonArray = dao.qryGetArticlesForJournal(journalISSN);
+		} catch (Exception e) { // Print out to the console if the call is not
+								// completed
+			e.printStackTrace();
+		}
+
+		return jsonArray; // Returns the output from the SQL query
+	}
+
+	/**
 	 * Reads in the character stream data and converts it to string
-	 * @param rd The character stream of type Reader
+	 * 
+	 * @param rd
+	 *            The character stream of type Reader
 	 * @return The converted character stream as a string
-	 * @throws IOException Throws this if it cannot read the data
+	 * @throws IOException
+	 *             Throws this if it cannot read the data
 	 */
 	private static String readAll(Reader rd) throws IOException {
 		StringBuilder sb = new StringBuilder();
@@ -75,13 +287,19 @@ public class ApiGetMethods {
 
 	// Buffers in the JSON string from the URL
 	/**
-	 * Method to buffer in the data from the URL as a string, then converts it to a JSON array
-	 * @param url The URL that the data is being read from
+	 * Method to buffer in the data from the URL as a string, then converts it
+	 * to a JSON array
+	 * 
+	 * @param url
+	 *            The URL that the data is being read from
 	 * @return JSON encoded data of the string data
-	 * @throws IOException Throws this if the data cannot be read
-	 * @throws JSONException Throws this if there is a problem with the JSON
+	 * @throws IOException
+	 *             Throws this if the data cannot be read
+	 * @throws JSONException
+	 *             Throws this if there is a problem with the JSON
 	 */
-	public static JSONArray readJsonFromUrl(String url) throws IOException,	JSONException {
+	public static JSONArray readJsonFromUrl(String url) throws IOException,
+			JSONException {
 		InputStream is = new URL(url).openStream();
 		try {
 			BufferedReader rd = new BufferedReader(new InputStreamReader(is,
